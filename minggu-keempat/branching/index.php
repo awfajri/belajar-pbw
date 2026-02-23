d409c33e59496ca2d7896fc43f99625f03a90856
awfajrii <awfajrii@gmail.com>
2026-02-23 11:44:54 +0700

    belajar branching dan switch

---
+++ minggu-keempat/branching/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Belajar Branching</title>
+  </head>
+  <body>
+    <?php
+    $nilai = 85;
+    $kehadiran = 90;
+
+    if ($nilai >=70 && $kehadiran >=75) {
+      echo "Selamat, Anda lulus!";
+    } else {
+      echo "Maaf, Anda tidak lulus.";
+    }
+
+    ?>
+  </body>
+</html>
